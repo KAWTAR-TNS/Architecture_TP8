3febd02485335e82d188ec65d8d13051ca6f8451
kawtar-tns <tanassakawtar@gmai.com>
2025-10-26 21:47:14 +0100

    first

---
+++ src/main/java/com/example/plateforme_paiement/PlateformePaiementApplication.java
@@ -0,0 +1,13 @@
+package com.example.plateforme_paiement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformePaiementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformePaiementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plateforme_paiement/PlateformePaiementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plateforme_paiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformePaiementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
